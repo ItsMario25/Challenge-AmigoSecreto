31ab1c3b9f6e472b5116e7cae6e42f1cb2fc4837
ItsMario25 <mmariohernandez58@gmail.com>
2025-08-08 17:23:34 -0500

    funciones agregarAmigo y mostrarListaAmigos

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, escribe un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    mostrarListaAmigos();
+
+    input.value = "";
+    input.focus();
+}
+
+function mostrarListaAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
